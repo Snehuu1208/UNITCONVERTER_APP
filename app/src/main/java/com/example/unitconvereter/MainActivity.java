90e5b6a227c787e6a0c605db99ec6a4e35a6747a
Sneha <snehu6369@gmail.com>
2023-03-21 23:29:53 +0530

    unit converter

--- app/src/main/java/com/example/unitconvereter/MainActivity.java
+++ app/src/main/java/com/example/unitconvereter/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Thread thread=new Thread(){
             @Override
